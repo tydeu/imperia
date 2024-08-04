983dfe4a381f9b702b76b3bfa0df4c67aabf430c
tydeu <tydeu@hatpress.net>
2024-08-03 21:16:42 -0400

    test: richer `#guard_ir` diff

--- Imperia/Test/GuardIR.lean
+++ Imperia/Test/GuardIR.lean
@@ -4,8 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mac Malone
 -/
 import Lean.Elab.Command
+import Lean.Util.Diff
 
-open Lean Elab Command
+open Lean Diff Elab Command
 
 namespace Imperia
 
@@ -14,22 +15,40 @@ deriving instance BEq for IR.DeclInfo
 deriving instance BEq for ExternEntry
 deriving instance BEq for ExternAttrData
 
+def diffIRParams (actual expected : Array (IR.Param)) : Except String Unit := do
+  if h_sz : actual.size = expected.size then
+    for h : i in [0:actual.size] do
+      have : i < expected.size := h_sz ▸ h.upper
+      unless actual[i].ty == expected[i].ty do
+        throw s!"type mismatch at {i}: got {actual[i].ty}, expected {expected[i].ty}"
+  else
+    throw s!"arity mismatch: got {actual.size}, expected {expected.size}"
+
+def diffIRFnBody (afn xfn : Name) (abody xbody : IR.FnBody) : Except String Unit := do
+  unless abody == xbody do
+    let air := (toString abody).replace (afn.toString false) (xfn.toString false)
+    let alns := air.split (· == '\n') |>.toArray
+    let xlns := (toString xbody).split (· == '\n') |>.toArray
+    let diffs := diff xlns alns
+    unless diffs.all (fun ⟨act,_⟩ => act matches .skip) do
+      throw <| linesToString diffs
+
 def diffIRDecl (actual expected : IR.Decl) : Except String Unit := do
   match actual, expected with
-  | .fdecl _ aps aty abody ainfo, .fdecl _ xps xty xbody xinfo =>
-    unless aps == xps do
-      throw "parameter mismatch"
+  | .fdecl afn aps aty abody ainfo, .fdecl xfn xps xty xbody xinfo =>
+    if let .error e := diffIRParams aps xps then
+      throw s!"parameter mismatch: {e}"
     unless aty == xty do
-      throw "return type mismatch"
+      throw s!"return type mismatch: got {aty}, expected {xty}"
     unless ainfo == xinfo do
       throw "declaration info mismatch"
-    unless abody == xbody do
-      throw "body mismatch"
+    if let .error diff := diffIRFnBody afn xfn abody xbody then
+      throw s!"body mismatch:\n{diff}"
   | .extern _ aps aty xdata, .extern _ xps xty ydata =>
-    unless aps == xps do
-      throw "parameter mismatch"
+    if let .error e := diffIRParams aps xps then
+      throw s!"parameter mismatch: {e}"
     unless aty == xty do
-      throw "return type mismatch"
+      throw s!"return type mismatch: got {aty}, expected {xty}"
     unless xdata == ydata do
       throw "declaration info mismatch"
   | _, _ =>
